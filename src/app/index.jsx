978b421a8dd32be03249b50bd8f6e053efe276ed
P-Wolff <priscilawolff@hotmail.com.br>
2024-08-07 13:23:59 -0300

    subindo

---
+++ src/app/index.jsx
@@ -0,0 +1,14 @@
+import { Link } from 'react-router-dom';
+
+
+import './index.scss';
+
+
+export default function App() {
+  return (
+    <div className="pagina-app">
+      <h1>opa</h1>
+    </div>
+  );
+}
+
